f982ed89432331548057bbb0ef45ff669f37c113
wangjianyuan10 <wangjianyuan10@163.com>
2019-11-16 17:48:41 +0800

    chapter1 and chapter2 modified for new version wx 20191116

--- Chapter-02/startup.py
+++ Chapter-02/startup.py
@@ -6,30 +6,31 @@ import sys
 class Frame(wx.Frame):
 
     def __init__(self, parent, id, title):
-        print "Frame __init__"
+        print("Frame __init__")
         wx.Frame.__init__(self, parent, id, title)
 
 class App(wx.App):
 
     def __init__(self, redirect=True, filename=None):
-        print "App __init__"
+        print("App __init__")
         wx.App.__init__(self, redirect, filename)
 
     def OnInit(self):
-        print "OnInit"
+        print("OnInit")
         self.frame = Frame(parent=None, id=-1, title='Startup Window')
         self.frame.Show()
         self.SetTopWindow(self.frame)
-        print >> sys.stderr, "A pretend error message"
-        print "app name: <", self.GetVendorName(), ">"
+        print (sys.stderr, "A pretend error message")
+        print ("app name: <", self.GetVendorName(), ">")
         return True
 
     def OnExit(self):
-        print "OnExit"
+        print ("OnExit")
+        return True
 
 if __name__ == '__main__':
     app = App(redirect=True)
-    print "before MainLoop"
+    print("before MainLoop")
     fred = app.MainLoop()
-    print "after MainLoop", fred
+    print("after MainLoop", fred)
 
